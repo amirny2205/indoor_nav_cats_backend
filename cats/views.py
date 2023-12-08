58b6dfa6e48bd20d20f691a3b12a3b5d46a1e145
user <user@localhost.localdomain>
2023-12-09 02:36:51 +0300

    cats are now created only by non-anonymous users

--- cats/views.py
+++ cats/views.py
@@ -1,12 +1,26 @@
 from .models import CatModel
 from .serializers import CatSerializer
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class CatListCreate(generics.ListCreateAPIView):
     queryset = CatModel.objects.all()
     serializer_class = CatSerializer
 
+    def create(self, validated_data):
+        if validated_data.user.is_anonymous:
+            return Response("anonymous user is not allowed for cats creation", status=status.HTTP_400_BAD_REQUEST)
+        else:
+            validated_data.data["owner"] = validated_data.user.pk
+            serializer = self.get_serializer(data=validated_data.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+            
+ 
 
 class CatDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = CatModel.objects.all()
