a7eadcd3815eb2cd8498a393de2ae2b2d4158b90
user <user@localhost.localdomain>
2023-12-20 14:26:44 +0300

    environ overwrite=True for split settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,1 +1,17 @@
 from .base import *
+import environ
+
+env.read_env(env.str('ENV_PATH', '.env_local'), overwrite=True)
+
+SECRET_KEY = env('SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'incdb',
+        'USER': 'catsuser',
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
