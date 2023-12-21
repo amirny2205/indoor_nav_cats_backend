fa2f1e9e9f79a5294085a97d35c9fd590bc04e77
user <user@localhost.localdomain>
2023-12-21 18:33:06 +0300

    created message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class MessageModel(models.Model):
+    msg_to = models.ForeignKey(User, related_name='received_message', on_delete=models.CASCADE)
+    msg_from = models.ForeignKey(User, related_name='sent_message', on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.content
